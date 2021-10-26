82c406f8b62aceb7ed9cd06a2471ba4abcaf4950
hsh <tmdgus2568@gmail.com>
2021-10-26 12:28:50 +0900

    first commit

---
+++ WebContent/dbtest.jsp
@@ -0,0 +1,18 @@
+<%@ page import="uridongne.util.DBConnection" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+	new DBConnection().test();
+
+
+%>
+
+</body>
+</html>
---
+++ src/uridongne/util/DBConnection.java
@@ -0,0 +1,33 @@
+package uridongne.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DBConnection {
+
+	String DB_URL="jdbc:oracle:thin:@matching_medium?TNS_ADMIN=/Users/hsh/Desktop/kosta/uridongne_sub/Wallet_matching";
+	String DB_USER = "ADMIN";
+	String DB_PASSWORD = "Kosta226good";
+	public void test() throws ClassNotFoundException, SQLException {
+	
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+//		Security.addProvider(new oracle.security.pki.OraclePKIProvider());
+		Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		System.out.println(conn);
+		String sql = "select * from member";
+		Statement st = null;
+		ResultSet rs = null;
+		st = conn.createStatement();
+		rs = st.executeQuery(sql);
+		while(rs.next()) {
+			System.out.println(rs.getString(2));
+		}
+		rs.close();
+		st.close();
+		conn.close();
+	}
+	// dfdfdf
+}
